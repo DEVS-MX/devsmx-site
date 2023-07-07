1f058321eb32ada5a99cfe32d413d8581c7ef2d9
Ariel Loza <ariel@itnora.com>
2023-07-07 11:10:42 -0600

    Update Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3030,
+    host: true
+  },
+  integrations: [tailwind()]
+});
